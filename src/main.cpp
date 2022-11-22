1ca58609e618cc7af90cfdd1d27292dccc989106
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-11-21 23:53:15 -0300

    Add menu
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,9 @@
 #include <thread>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
-	Engine run;
+Engine run;
 
-	std::thread t1(&Engine::generateContagion, run, 13, 13);
-	std::thread t2(&Engine::rollDeath, run);
-
-	run.createNodesAndVerticesWithMap();
-
-	// std::thread t3(&Engine::vivaSUS, run, 3,35);
-	// std::thread t5(&Engine::vivaSUS, run, 35,3);
-
-	std::thread t6(&Engine::imunidadeDeRebanho, run, MAX_VALUE*33+37);
-	statusGlobal[MAX_VALUE*33+37] = 190;
-
+void imprime(){
 	while( 1 ) 
 	{
 		std::cout << run.imprimirPlano();
@@ -33,7 +21,85 @@ int main(int argc, char *argv[])
 		sleep(1);
 		system("clear");
 
-	}
+	}	
+}
+
+void case1() {
+	run.createNodesAndVerticesWithMap();
+	std::thread t1(&Engine::generateContagion, run, 13, 13);
+	std::thread t2(&Engine::rollDeath, run);
+	std::thread t3(&Engine::vivaSUS, run, 3,35);
+
+	imprime();
+}
+
+void case2() {
+	run.createNodesAndVerticesWithMap();
+	std::thread t4(&Engine::generateContagion, run, 13, 13);
+	std::thread t5(&Engine::rollDeath, run);
+	std::thread t6(&Engine::vivaSUS, run, 3,35);
+	std::thread t7(&Engine::vivaSUS, run, 35,3);
+
+	imprime();
+}
+
+void case3() {
+	run.createNodesAndVerticesWithMap();
+	std::thread t11(&Engine::generateContagion, run, 13, 13);
+	std::thread t22(&Engine::rollDeath, run);
+	std::thread t66(&Engine::imunidadeDeRebanho, run, MAX_VALUE*33+37);
+	statusGlobal[MAX_VALUE*33+37] = 190;
+
+
+	imprime();
+}
+
+
+int main(int argc, char *argv[])
+{
+
+
+  int menu{0};
+
+	Engine run;
+	run.createNodesAndVerticesWithMap();
+
+    system("clear");
+
+     // coloque false para testar as buscas nos nós
+
+
+        std::cout << "Escolha a configuração da simulação: " << '\n';
+        std::cout << "1. BFS - 1 contaminado e 1 agente de saúde" << '\n';
+        std::cout << "2. BFS - 1 contaminado e 2 agentes de saúde " << '\n';
+        std::cout << "3. DFS - 1 contaminado e 1 agentes de saúde" << '\n';
+
+        std::cout << "0. Sair" << '\n';
+
+        std::cin >> menu;
+
+        system("clear");
+		
+        switch (menu) 
+		{
+            case 1 :
+				case1();
+                break;
+            case 2 :
+				case2();
+                break;
+            case 3 :
+				case3();
+                break;
+            case 0 :
+                return 0;
+                break;
+            default:
+                std::cout << "Escolha invalida!" << '\n';
+				sleep(3);
+				return 0;
+
+        }
 
 	return 0;
 
